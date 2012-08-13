f709badb0fffefc6c792235bd606168d1e41c892
chew no ill <chew.no.ill@gmail.com>
2012-08-13 19:38:59 -0400

    TOP SECRET!!!! DO NOT TOUCH!!1

--- src/core_dos/shared/secret.java
+++ src/core_dos/shared/secret.java
@@ -1,7 +1,9 @@
 package core_dos.shared;
 
 public final class secret {
-	static String SQL_SERVER = "change";
-	static String SQL_PASSWORD = "this";
-
+	static String SQL_SERVER = "not a real place";
+	static String SQL_DATABASE = "";
+	static String SQL_USER = "not a real user";
+	static String SQL_PASSWORD = "1234";//this one is real
+	
 }
