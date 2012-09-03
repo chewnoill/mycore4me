98055ec1974e1e60a6fccba667db39d01f8a2eb1
chewnoill <github@williamcohen.com>
2012-09-03 04:02:17 -0300

    Update src/core_dos/shared/secret.java
    deautherize 

--- src/core_dos/shared/secret.java
+++ src/core_dos/shared/secret.java
@@ -1,11 +1,11 @@
 package core_dos.shared;
 
 public final class secret {
-	public static String SQL_SERVER = "mycore4me.db.7935234.hostedresource.com";
-	public static String SQL_DATABASE = "mycore4me";
-	public static String SQL_USER = "mycore4me";
-	public static String SQL_PASSWORD = "p9yrq@#HJFD";//this one is real
-	public static String CLIENT_ID ="893258520650.apps.googleusercontent.com";
-	public static String API_KEY = "7tlKlUWV7TD0lfdphxn6ENYN";
+	public static String SQL_SERVER = "";
+	public static String SQL_DATABASE = "";
+	public static String SQL_USER = "";
+	public static String SQL_PASSWORD = "";
+	public static String CLIENT_ID ="";
+	public static String API_KEY = "";
 
 }
